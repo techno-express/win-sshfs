68c9b667937594bca82833c460b8c7b5da841a9b
thb42 <thomas.bauer.info@googlemail.com>
2014-11-26 22:32:11 +0100

    new file:   Sshfs/GuiBackend/IServiceFisshBone.cs 	new file:   Sshfs/GuiBackend/IServiceFisshBone.netmodule 	new file:   Sshfs/GuiBackend/ServiceFisshBone.cs 	new file:   Sshfs/GuiBackend/ServiceFisshBone.netmodule
    first example of an interface to access the backend + assembly inforamtion

---
+++ Sshfs/Sshfs/GuiBackend/IServiceFisshBone.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Sshfs.GuiBackend
+{
+    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Schnittstellennamen "IService1" sowohl im Code als auch in der Konfigurationsdatei ändern.
+    [ServiceContract]
+    public interface IServiceFisshBone
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        void Mount();
+
+        [OperationContract]
+        void UMount();
+
+        [OperationContract]
+        void add();
+
+        [OperationContract]
+        void remove();
+
+        [OperationContract]
+        void Connect();
+
+        [OperationContract]
+        void Disconnect();
+
+        [OperationContract]
+        void Change();
+
+
+
+    }
+}
---
+++ Sshfs/Sshfs/GuiBackend/ServiceFisshBone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Sshfs.GuiBackend
+{
+    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Klassennamen "Service1" sowohl im Code als auch in der Konfigurationsdatei ändern.
+    public class ServiceFisshBone : IServiceFisshBone
+    {
+
+        //private readonly StringBuilder _balloonText = new StringBuilder(255);
+        //private readonly List<String> _configVars = new List<String>();
+        //private readonly Regex _regex = new Regex(@"^New Drive\s\d{1,2}$", RegexOptions.Compiled);
+        /*
+        //private readonly Queue<SftpDrive> _suspendedDrives = new Queue<SftpDrive>();
+        //private readonly List<SftpDrive> _drives = new List<SftpDrive>();
+       
+        public FisshBone()
+        { }
+
+
+        public string MethodA()
+        {
+            Console.WriteLine("MethodA has been invoked!");
+            return "Invoked Succesfully!";
+        }
+*/
+
+
+
+        public void DoWork()
+        {
+        }
+    }
+}
