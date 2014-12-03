f6d1b9e48ff1b621b07ade662467b5019c3cd3a2
bjoe-phi <bjoern-philipp.kreuzkamp@tu-ilmenau.de>
2014-12-03 01:17:42 +0100

    adding cmd-prototype
    Adding a prototype for command-line-interface.
    It can receive arguments and complain about wrong usage.

---
+++ Sshfs/cmd_interface/fissh_cmdline_interface/Program.cs
@@ -0,0 +1,98 @@
+﻿/*
+Copyright (c) 2014 bjoe-phi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+// This Software is a interface to controll FiSSH over Command-Line
+
+
+using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+
+
+namespace fissh_cmdline_interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // arguments get parsed by contructor; all parameters will be available in the new object
+            var cmdline_parameters = new fissh_command.fissh_command_expression(args);
+
+
+            // Everything after this line ist just for testing
+            #region Testing
+            Console.WriteLine("");
+            Console.WriteLine("=== Testbench ===");
+
+            switch(cmdline_parameters.keyword) 
+            {
+                case (byte)fissh_command.fissh_command_keywords.mount:
+                    if (!cmdline_parameters.parameter_host.is_set_flag) 
+                    {
+                        if (cmdline_parameters.parameter_folderlist.is_set_flag)
+                        {
+                            Console.WriteLine("You want me to mount {0} on server {1}.", cmdline_parameters.parameter_folderlist.get(), cmdline_parameters.parameter_servername.get());
+                        }
+                        else {
+                            Console.WriteLine("You want me to mount the complet server {0}.", cmdline_parameters.parameter_servername.get());
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("You want me to mount a server with the URL {0} on Port {1}.", cmdline_parameters.parameter_host.get(), cmdline_parameters.option_port.get());
+                        Console.WriteLine("Path on Server: {0}", cmdline_parameters.option_path.get());
+                        Console.WriteLine("Loginname: {0}", cmdline_parameters.option_login_name.get());
+                        Console.WriteLine("Authentifikation: {0}", cmdline_parameters.option_key.get());
+                    }
+                    break;
+
+                case (byte)fissh_command.fissh_command_keywords.umount:
+                    Console.WriteLine("You want to unmount.");
+                    break;
+
+                case (byte)fissh_command.fissh_command_keywords.status:
+                    Console.WriteLine("You ask for a status.");
+                    break;
+
+                case (byte)fissh_command.fissh_command_keywords.help:
+                    Console.WriteLine("HELP!!!");
+                    break;
+
+
+                default: 
+                    Console.WriteLine("Unknown keyword");
+                    break;
+            }
+            Console.WriteLine(" ");
+            #endregion
+
+        }
+
+
+
+    }
+}
+
---
+++ Sshfs/cmd_interface/fissh_cmdline_interface/fissh_command.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+using NDesk.Options;//for getopt
+
+
+
+namespace fissh_command
+{
+
+    enum fissh_command_keywords : byte { _no_match, mount, umount, status, help };
+
+    public class fissh_command_expression
+    {
+
+        public byte keyword;    //keyword comes from the first argument, eg. "mount", "umount", "help", "status"
+        
+        public bool any_option_is_set_flag;     //will ne true, if there is any option set
+        
+        // options are all arguments which look like "-l user" or "-p 22"
+        public option option_login_name = new option();
+        public option option_port = new option();
+        public option option_key = new option();
+        public option option_path = new option();
+        public option option_drive = new option();
+        public option option_virtual_drive = new option();
+        
+        // parameters are arguments without "-" or "/" as first letter, like "servername" or "folder1,folder2,folderx"
+        public option parameter_host = new option();
+        public option parameter_servername = new option();
+        public option parameter_folderlist = new option();
+
+
+
+        /// <summary>
+        /// contructor; it will parse the given arguments
+        /// </summary>
+        /// <param name="args">arguments to parse</param>
+        public fissh_command_expression(string[] args)
+        {
+            fetch_keyword(args);    //parse first argument
+
+            // delete the fist argument, which should be the keyword
+            List<string> args_list = args.ToList();
+            args_list.RemoveAt(0);  //delete first element
+            args = args_list.ToArray();
+            
+            // parse other arguments
+            // first all options, then parameters
+            fetch_parameters(fetch_options(args));  
+        }
+
+
+        /// <summary>
+        /// Instanzes of this class are used to present Option or Parameters
+        /// </summary>
+        public class option
+        {
+            public string value; // to save the parameter
+            public bool is_set_flag;    // will be true, if the parameter is used 
+
+            public option()
+            {
+                is_set_flag = false;
+                value = null;
+            }
+
+            public void set(string str)
+            {
+                is_set_flag = true;
+                value = str;
+            }
+
+            public string get()
+            {
+                return value;
+            }
+        }
+
+
+        #region METHODS
+        /// <summary>
+        /// looks at the first Argument and looks for a matching keyword
+        /// </summary>
+        /// <param name="args">arguments to parse</param>
+        private void fetch_keyword(string[] args)
+        {   
+            //if there are no arguments, exit
+            if (args.Length == 0)
+            {
+                fissh_print.error_message("no arguments found");
+                Environment.Exit(-1);
+            }
+
+            //else: compare first argument with defined keywords
+            else
+            {
+                switch (args[0])
+                {
+                    case "mount": keyword = (byte)fissh_command_keywords.mount; return;
+                    case "umount": keyword = (byte)fissh_command_keywords.umount; return;
+                    case "status": keyword = (byte)fissh_command_keywords.status; return;
+                    case "help": keyword = (byte)fissh_command_keywords.help; return;
+
+                    default: 
+                        if(args[0].Substring(0,1) == "-" || args[0].Substring(0,1) == "/")
+                        {
+                            fissh_print.error_message("no keyword found");
+                        }
+                        else
+                        {
+                            fissh_print.error_message("no matching keyword found");
+                        }
+
+                        Environment.Exit(-1);
+                        return;
+
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// get all options from the arguments
+        /// It uses NDesk.Options for parsing 
+        /// 
+        /// </summary>
+        /// <param name="args">arguments to parse</param>
+        /// <returns>those arguments, which are no options will be return</returns>
+        private List<string> fetch_options(string[] args)
+        {
+            List<string> extra_parameters; // to store the arguments, which are not options
+
+            any_option_is_set_flag = false;
+
+            // OptionSet() from NDesk.Options
+            // all possible options are definde here
+            // {"option-switch", "Text for Help(not used here)", what to do when found}
+            var p = new OptionSet() {
+		   	    { "l=", "Login Name", v => {option_login_name.set(v); any_option_is_set_flag = true;} },
+                { "p=", "Port", (int v) => {option_port.set(v.ToString()); any_option_is_set_flag = true;}  },
+                { "k=", "Key for Authentification", v => {option_key.set(v); any_option_is_set_flag = true;}  },
+                { "s=", "Path on Server", v => {option_path.set(v); any_option_is_set_flag = true;}  },
+                { "d=", "Driveletter", v => {option_drive.set(v); any_option_is_set_flag = true;}  },
+                { "v=", "Virtualdrive", v => {option_virtual_drive.set(v); any_option_is_set_flag = true;}  },
+                //  {"<>", v => Console.WriteLine("unknown option: {0}", v) },
+		    };
+
+
+            try
+            {
+                extra_parameters = p.Parse(args);   //options are parsed, remaining arguments are returned
+                
+                //looking for unknown options in remaining arguments
+                //if there is one, close programe
+                for (int i = 0; i < extra_parameters.Count; i++)
+                {
+                    if (extra_parameters[i].Substring(0, 1) == "-" || extra_parameters[i].Substring(0, 1) == "/")
+                    {
+                        Console.WriteLine("fissh: unknown option {0}", extra_parameters[i]);
+                        fissh_print.error_message("unknown option " + extra_parameters[i]);
+                        Environment.Exit(-1);
+                    }
+                }
+
+                //otherwise return remainig arguments
+                return extra_parameters;
+
+            }
+            catch (OptionException e)   //eg. when Port is no INT
+            {
+                //Console.Write("fissh: ");
+                //Console.WriteLine(e.Message);
+                //Console.WriteLine("Try `fissh help' for more information.");
+                fissh_print.error_message(e.Message);
+                Environment.Exit(-1);
+                return null; //useless, but VS is complaning whithout return
+            }
+
+
+        }
+
+
+        /// <summary>
+        /// for parsing arguments, which are neither a keyword nor options
+        /// those arguments are parameters like servername etc.
+        /// </summary>
+        /// <param name="parameters">remainig arguments</param>
+        private void fetch_parameters(List<string> parameters)
+        {
+            switch (keyword)    //uses of arguments depends on the keyword 
+            {
+                case (byte)fissh_command_keywords.mount:
+
+
+                    //User wants to mount a new server
+                    if (any_option_is_set_flag)
+                    {
+                        int i_start, i_end;     //indices to isolate host from username and port (user@host:port)
+                        string puffer_login_name, puffer_port;
+
+                        // If there is no parameter
+                        if (parameters.Count() == 0)
+                        {
+                            fissh_print.error_message("fissh: missing parameter");
+                            Environment.Exit(-1);
+                        }
+
+                        //isolate username
+                        i_start = parameters[0].IndexOf("@");
+                        if (i_start == -1)      //if there is no "@"
+                        {
+                            puffer_login_name = null;
+                            i_start = 0;        //so hostname starts with first char
+                        }
+                        else
+                        {
+                            puffer_login_name = parameters[0].Substring(0, i_start);
+                            i_start++;
+                        }
+
+                        //isolate portnumber
+                        i_end = parameters[0].IndexOf(":");
+                        if (i_end == -1)    //if ther is no ":"
+                        {
+                            puffer_port = null;
+                            i_end = parameters[0].Length - 1;   //so hostname ends with last char
+                        }
+                        else
+                        {
+                            puffer_port = parameters[0].Substring(i_end + 1);
+                        }
+
+
+                        //if username is not already set by option, use username from parameter 
+                        if (!option_login_name.is_set_flag)
+                        {
+                            option_login_name.set(puffer_login_name);
+                        }
+
+                        //if portnumber is not already set by option, use portnumber from parameter 
+                        if (!option_port.is_set_flag)
+                        {
+                            option_port.set(puffer_port);
+                        }
+
+                        //isolate hostname
+                        parameter_host.set(parameters[0].Substring(i_start, i_end - i_start + 1));
+                    }
+
+                    //User wants to mount an existing server
+                    else
+                    {
+                        int i = parameters.Count();
+                        if (i >= 1)
+                        {
+                            parameter_servername.set(parameters[0]);    //first argument is servername
+                        }
+                        if (i >= 2)
+                        {
+                            parameter_folderlist.set(parameters[1]);    //if there is a secound, it is the folderlist
+                        }
+
+                    }
+                    break;
+                default: return;
+            }
+        }
+        #endregion
+    }
+
+    public static class fissh_print 
+    {
+        public static void error_message(string error_message)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("fissh: {0}", error_message);
+            Console.WriteLine();
+            Console.WriteLine("Usage:");
+            //Console.WriteLine("");
+            Console.WriteLine("To mount a drive");
+            Console.WriteLine("     fissh mount SERVERNAME [FOLDERLIST]");
+            Console.WriteLine("     fissh mount [-l user] [-p port] [-k password=...| public_key=...]"); 
+            Console.WriteLine("                 -s PATH -d DRIVE-LETTER [user@]host[:port]");
+            Console.WriteLine("");
+            Console.WriteLine("To unmount a drive");
+            Console.WriteLine("     fissh umount DRIVE-LETTER | -v VIRTUAL_DRIVE_FOLDER");
+            Console.WriteLine("     fissh umount SERVERNAME [FOLDERLIST]");
+            Console.WriteLine("");
+            Console.WriteLine("Ask if a drive is mounted");
+            Console.WriteLine("     fissh status DRIVE-LETTER | -v VIRTUAL_DRIVE_FOLDER");
+            Console.WriteLine("     fissh status SERVERNAME FOLDER");
+            Console.WriteLine("");
+            Console.WriteLine("For more informations");
+            Console.WriteLine("     fissh help");
+            Console.WriteLine("");
+
+        }
+    }
+
+}
