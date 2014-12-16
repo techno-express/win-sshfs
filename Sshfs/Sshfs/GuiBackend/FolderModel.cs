c14391026ea7ec247bc859129ba9a21ae2ff74b4
thb42 <thomas.bauer.info@googlemail.com>
2014-12-17 00:19:31 +0100

    alternativ implementation to replace SftDrive_for_hierachy

---
+++ Sshfs/Sshfs/GuiBackend/FolderModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sshfs.GuiBackend
+{
+    class FolderModel
+    {
+        public Boolean use_global_login;
+        /*Guid FolderID*/
+        public string Folder;
+        public string Mountpoint;
+        public string Username;
+        public string Passphrase;
+    }
+}
---
+++ Sshfs/Sshfs/GuiBackend/ServerModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sshfs.GuiBackend
+{
+    class ServerModel
+    {
+#region ATTRIBUTES
+        public String Name;
+        public Guid ServerID;
+
+        public String Notes;
+
+        public String Host;
+        public int Port{get; set;};
+
+        public string PrivateKey;
+        public string Password { get; set; }
+        public string Passphrase { get; set; }
+        public string Username { get; set; }
+
+        public ConnectionType ConnectionType {get; set; }
+
+        //Folders for a Server kann be stored as List of Foldermodels
+        // or as List of Strings
+        List<FolderModel> Folders= new List<FolderModel>();
+        /*List<string> Folders = new List<string>();*/
+
+#endregion
+
+    }
+}
