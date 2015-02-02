67fe06c193fffe51a764cbf730c5aacf295b959a
thb42 <thomas.bauer.info@googlemail.com>
2015-02-02 14:37:05 +0100

    adding String[] args

--- Sshfs/GUI/WindowsFormsApplication2/Program.cs
+++ Sshfs/GUI/WindowsFormsApplication2/Program.cs
@@ -14,15 +14,18 @@ namespace GUI_WindowsForms
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(String[] args)
         {
+            //if argument for no gui start is given, set this false
+            bool StartGUI = true;
+
             //start ServiceHost
             ProcessStartInfo startEXE = new ProcessStartInfo();
             startEXE.FileName = "ConsoleApplication1.exe";
             startEXE.WorkingDirectory = @"C:\Users\thomas\Documents\GitHub\win-sshfs\Sshfs\FiSSHBone\bin\Debug";
             startEXE.ErrorDialog = true;
 
-            if (Process.Start(startEXE) != null)
+            if (Process.Start(startEXE) != null && StartGUI)
             {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
