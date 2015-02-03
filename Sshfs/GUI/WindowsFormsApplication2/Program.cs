cdcbaebff8a3828f1381779cf13c50a131de22d1
bjoe-phi <bjoern-philipp.kreuzkamp@tu-ilmenau.de>
2015-02-03 14:07:22 +0100

    Changed GUI startup routine. GUI checks if bone is running

--- Sshfs/GUI/WindowsFormsApplication2/Program.cs
+++ Sshfs/GUI/WindowsFormsApplication2/Program.cs
@@ -19,19 +19,45 @@ namespace GUI_WindowsForms
             //if argument for no gui start is given, set this false
             bool StartGUI = true;
 
-            //start ServiceHost
-            ProcessStartInfo startEXE = new ProcessStartInfo();
-            startEXE.FileName = "FiSSHBone.exe";
-            startEXE.WorkingDirectory = "";
-            startEXE.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            startEXE.ErrorDialog = true;
+            IServiceFisshBone bone_server = IPCConnection.ClientConnect();
+            //start ServiceHost if it is not running
+            try
+            {
+                bone_server.listAll();
+            }
+            catch
+            {
+                ProcessStartInfo startEXE = new ProcessStartInfo();
+                startEXE.FileName = "FiSSHBone.exe";
+                startEXE.WorkingDirectory = "";
+                startEXE.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startEXE.ErrorDialog = true;
+                if (Process.Start(startEXE) == null)
+                {
+                    Environment.Exit(1);
+                }
+            }
 
-            if (Process.Start(startEXE) != null && StartGUI)
+            // Loop: wait until IPC server is online
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    bone_server.listAll();
+                }
+                catch
+                {
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+
+
+            if (StartGUI)
             {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new FiSSHForm());
             }
-        }
+    }
     }
 }
