7c9016e11ae3f98b7a380c95189a3ca68a6201ce
bjoe-phi <bjoern-philipp.kreuzkamp@tu-ilmenau.de>
2015-02-03 12:37:42 +0100

    Change assembly names

--- Sshfs/GUI/WindowsFormsApplication2/Properties/Settings.Designer.cs
+++ Sshfs/GUI/WindowsFormsApplication2/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GUI.Properties {
+namespace FiSSHGui.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
