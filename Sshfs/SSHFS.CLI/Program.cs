304188c733d77ed4337351fd84f5038a6a19a226
Asad Saeeduddin <masaeedu@gmail.com>
2017-06-30 17:22:57 -0400

    Add multi-auth

--- Sshfs/SSHFS.CLI/Program.cs
+++ Sshfs/SSHFS.CLI/Program.cs
@@ -1,11 +1,15 @@
 ﻿using CommandLine;
 using DokanNet;
 using Renci.SshNet;
+using Renci.SshNet.Common;
 using Sshfs;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SSHFS.CLI
@@ -13,22 +17,38 @@ namespace SSHFS.CLI
     class Options
     {
         // Paths
-        [Option('d', "drive-letter", Required = true, HelpText = "Drive letter to mount the remote SFTP path under")]
+        [Option('d', "drive-letter",
+            Required = true,
+            HelpText = "Drive letter to mount the remote SFTP path under")]
         public char DriveLetter { get; set; }
-        [Option('r', "path", Required = true, HelpText = "Absolute path of directory to be mounted from remote system")]
-        public string Root { get; set; }
+        [Option('r', "path",
+            Required = true,
+            HelpText = "Absolute path of directory to be mounted from remote system")]
+        public string Path { get; set; }
 
         // Remote host
-        [Option('h', "host", Required = true, HelpText = "IP or hostname of remote host")]
+        [Option('h', "host",
+            Required = true,
+            HelpText = "IP or hostname of remote host")]
         public string Host { get; set; }
-        [Option('p', "port", Required = false, HelpText = "SSH service port on remote server")]
-        public int? Port { get; set; }
+        [Option('p', "port",
+            Required = false, DefaultValue = 22,
+            HelpText = "SSH service port on remote server")]
+        public int Port { get; set; }
 
         // Auth
-        [Option('u', "username", Required = true, HelpText = "Username")]
+        [Option('u', "username",
+            Required = true,
+            HelpText = "Name of SSH user on remote system")]
         public string Username { get; set; }
-        [Option('x', "password", Required = false, HelpText = "Password")]
+        [Option('x', "password",
+            Required = false,
+            HelpText = "SSH user's password, if password-based or keyboard-interactive auth should be attempted")]
         public string Password { get; set; }
+        [OptionArray('k', "private-keys",
+            Required = false,
+            HelpText = "SSH user's private key(s), if key-based auth should be attempted")]
+        public string[] Keys { get; set; }
     }
 
     class Program
@@ -38,19 +58,83 @@ namespace SSHFS.CLI
             var options = new Options();
             Parser.Default.ParseArgumentsStrict(args, options);
 
-            var auth = options.Port.HasValue
-                ? new KeyboardInteractiveConnectionInfo(options.Host, options.Port.Value, options.Username)
-                : new KeyboardInteractiveConnectionInfo(options.Host, options.Username);
+            var auths = GetAuthMechanisms(options);
+
+            var fs = auths
+                .Select(auth => AttemptConnection(auth.Item1, auth.Item2, options.Path))
+                .FirstOrDefault(result => result != null);
+
+            if (fs == null)
+                throw new InvalidOperationException("Could not connect to server with any known authentication mechanism");
+
+            fs.Mount($"{options.DriveLetter}");
+        }
+
+        static IEnumerable<(string, ConnectionInfo)> GetAuthMechanisms(Options options)
+        {
+            var auths = new List<(string, ConnectionInfo)>();
+
+            if (options.Keys != null)
+            {
+                auths.AddRange(new(string, ConnectionInfo)[] {
+                    ("private key", PrivateKeyConnectionInfo(options))
+                });
+            }
+
+            if (options.Password != null)
+            {
+                auths.AddRange(new(string, ConnectionInfo)[] {
+                    ("password", new PasswordConnectionInfo(options.Host, options.Port, options.Username, options.Password)),
+                    ("keyboard-interactive", KeyboardInteractiveConnectionInfo(options))
+                });
+            }
+
+            return auths;
+        }
+
+        static PrivateKeyConnectionInfo PrivateKeyConnectionInfo(Options options)
+        {
+            var pkFiles = options.Keys.Select(k =>
+            {
+                var match = Regex.Match(k, @"(?<keyfile>\S+)(:?\s+ (?<passphrase>\S+))?");
+                var keyfile = match.Groups["keyfile"];
+                var passphrase = match.Groups["passphrase"];
+
+                return passphrase.Success ? new PrivateKeyFile(keyfile.Value, passphrase.Value) : new PrivateKeyFile(keyfile.Value);
+            });
+
+            return new PrivateKeyConnectionInfo(options.Host, options.Port, options.Username, pkFiles.ToArray());
+        }
+
+        static KeyboardInteractiveConnectionInfo KeyboardInteractiveConnectionInfo(Options options)
+        {
+            var auth = new KeyboardInteractiveConnectionInfo(options.Host, options.Port, options.Username);
 
             auth.AuthenticationPrompt += (sender, e) =>
             {
-                foreach (var p in e.Prompts.Where(p => p.Request.StartsWith("Password:")))
+                var passPrompts = e.Prompts
+                    .Where(p => p.Request.StartsWith("Password:"));
+                foreach (var p in passPrompts)
                     p.Response = options.Password;
             };
-            var sftpFS = new SftpFilesystem(auth, options.Root);
-            sftpFS.Connect();
-            sftpFS.Mount($"{options.DriveLetter}");
-            Console.ReadLine();
+
+            return auth;
+        }
+
+        static SftpFilesystem AttemptConnection(string authType, ConnectionInfo connInfo, string path)
+        {
+            try
+            {
+                var sftpFS = new SftpFilesystem(connInfo, path);
+                sftpFS.Connect();
+                Console.WriteLine($"Successfully authenticated with {authType}.");
+                return sftpFS;
+            }
+            catch (SshAuthenticationException)
+            {
+                Console.WriteLine($"Failed to authenticate using {authType}, falling back to next auth mechanism if available.");
+                return null;
+            }
         }
     }
 }
