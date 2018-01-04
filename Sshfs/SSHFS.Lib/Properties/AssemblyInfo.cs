efd912d86a2d8c6f5338dbd1890d3b1693c5664c
techno-express <technoexpressnet@gmail.com>
2018-01-03 23:39:26 -0500

    Cosmetic changes, added msi installers, one installs just Windows SshFS Command Line version and other bundles an msi that downloads and install latest dokan and VS 2017 redistribution if not installed.

--- Sshfs/SSHFS.Lib/Properties/AssemblyInfo.cs
+++ Sshfs/SSHFS.Lib/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SSHFS.Lib")]
+[assembly: AssemblyTitle("Windows SshFS Library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SSHFS.Lib")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Windows SshFS Library")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
